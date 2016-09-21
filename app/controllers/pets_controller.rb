c7d59e5c93f689e36b9aab58bc6625f7c17855f4
Shawn LaCombe <lacombeshawn@yahoo.com>
2016-09-20 19:19:43 -0500

    Add bootstrap gem

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -69,6 +69,6 @@ class PetsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pet_params
-      params.require(:pet).permit(:animal_type, :name, :age)
+      params.require(:pet).permit(:animal_type, :name, :age, :owner_id)
     end
 end
